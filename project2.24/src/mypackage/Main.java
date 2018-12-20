50611270ad35f1678c49bf4569031654dc2a71de
Danil Komyshev <danil.komyshev777@gmail.com>
2018-12-20 18:48:11 +0400

    1

--- project2.24/src/mypackage/Main.java
+++ project2.24/src/mypackage/Main.java
@@ -17,10 +17,8 @@ public class Main {
         Integer n = scan.nextInt();
         int[] a = new int[n];
         for (int i = 0; i < a.length; i++){
-                a[i]=(int)(Math.random()*10);
-            }
-        for (int i = 0; i < a.length; i++){
-                System.out.print(a[i]+" ");
-            }
+            a[i]=(int)(Math.random()*10);
+            System.out.print(a[i]+" ");
+        }
     }
 }
