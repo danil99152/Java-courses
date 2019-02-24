b4e11535e4055501af5fc9c94c2967f27edcf5b6
Danil Komyshev <danil.komyshev777@gmail.com>
2019-02-24 21:07:53 +0400

    upd 4.9

---
+++ 4.9/src/mypackage/Word.java
@@ -0,0 +1,35 @@
+package mypackage;
+
+public class Word {
+    String value;
+
+    public Word(String string) {
+        value = string;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Word)) return false;
+
+        Word word = (Word) o;
+
+        if (!value.equals(word.value)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
